387b1ffdf2ffb60a8a0eb062e9382349ba3b909a
ilya <107561657+Sicrown@users.noreply.github.com>
2022-12-27 19:55:40 +0300

    Create User.java

---
+++ HomeWork/User.java
@@ -0,0 +1,14 @@
+package Seminar_6.HomeWork;
+
+public class User {
+    private String name;
+	
+	public User(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+		
+}
